5d9305da88936ab4ef0966dbb83b6c8c0ca2fdbb
Pain Killer <gachungasamuel53@gmail.com>
2022-06-15 02:20:56 +0300

    hope this works

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   const [movies, setMovies] = useState([])
 
   useEffect (() => {
-    searchMovies("Batman")
+    searchMovies("Superman")
   }, []);
 
   const searchMovies = async (title) => {
